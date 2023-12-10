aa5c1533edfe7e7bc4782fab46af32ebd7008f41
aamirr-ali <amirwide@gmail.com>
2023-12-10 13:18:47 +0000

    sm8350-common:Add properties for MATRIXX
    Signed-Off: Mrick343 <mrinmoy8429de@gmail.com>

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -6,8 +6,24 @@
 #include <android-base/logging.h>
 #include <android-base/properties.h>
 
+#include <cstdlib>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
+#include <android-base/properties.h>
+
+using android::base::GetProperty;
+using std::string;
+
+void property_override(string prop, string value)
+{
+    auto pi = (prop_info*) __system_property_find(prop.c_str());
+
+    if (pi != nullptr)
+        __system_property_update(pi, value.c_str(), value.size());
+    else
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+}
 
 using android::base::GetProperty;
 
@@ -98,4 +114,9 @@ void vendor_load_properties() {
         default:
             LOG(ERROR) << "Unexpected RF version: " << rf_version;
     }
+
+     // Misc
+    property_override("ro.matrixx.chipset", "Snapdragon® 888 5G");
+    property_override("ro.matrixx.device", "martini");
+    property_override("ro.matrixx.maintainer", "Mrick343");
 }
