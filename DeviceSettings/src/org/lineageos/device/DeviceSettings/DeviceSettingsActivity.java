crdroidandroid/android_device_oneplus_sm8350-common

sm8350-common: Add DeviceSettings
* Commit history: https://github.com/crdroidandroid/android_device_oneplus_sm8350-common/commits/11.0/DeviceSettings
* This is still dependent on harware/oplus for tri-state-key-calibrate binary
* Other changes:
  Changes for /proc/tristatekey/tri_state - https://github.com/crdroidandroid/android_hardware_oplus/commit/99f10d3d04f32a3b2e15181b6c359c7a2bafe1c2
  Adapted to S style - https://github.com/crdroidandroid/android_device_oneplus_sm8350-common/commit/82682c317dfcb9ce33815f3e91ed16ceac63dde8
  Added vibrator strength control - https://github.com/crdroidandroid/android_device_oneplus_sm8350-common/commit/c82a7b6a9c31ee64f1acecce6b06a8256a0b0678

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

DeviceSettings/src/org/lineageos/device/DeviceSettings/DeviceSettingsActivity.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2018-2022 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.device.DeviceSettings;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG = "DeviceSettings";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new DeviceSettings(), TAG).commit();
+    }
+}

Mrick343/android_device_oneplus_sm8350-common-pr

Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>

DeviceSettings/src/org/lineageos/device/DeviceSettings/DeviceSettingsActivity.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2018-2022 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.device.DeviceSettings;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class DeviceSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG = "DeviceSettings";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new DeviceSettings(), TAG).commit();
+    }
+}